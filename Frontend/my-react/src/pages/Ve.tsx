0edfaa6ea91b84f306f79705391c1d80642d032c
Tienanh325 <tienanh2k55@gmail.com>
2026-03-24 22:16:05 +0700

    ve admin

--- Frontend/my-react/src/pages/Ve.tsx
+++ Frontend/my-react/src/pages/Ve.tsx
@@ -1,11 +1,11 @@
-// import TicketAdminPage from "./TicketAdminPage";
+import TicketAdminPage from "./TicketAdminPage";
 
-// export default function Ve() {
-//   return (
-//     <TicketAdminPage
-//       category="all"
-//       title="Quản lý vé "
-//       description="Quản lý toàn bộ vé trong hệ thống, bao gồm vé máy bay, vé tàu hoả và vé khu vui chơi"
-//     />
-//   );
-// }
+export default function Ve() {
+  return (
+    <TicketAdminPage
+      category="all"
+      title="Quản lý vé "
+      description="Quản lý toàn bộ vé trong hệ thống, bao gồm vé máy bay, vé tàu hoả và vé khu vui chơi"
+    />
+  );
+}
