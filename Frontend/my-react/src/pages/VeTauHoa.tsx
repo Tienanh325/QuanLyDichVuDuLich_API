73831d1ecc940d0ff07ea10ea06960e16b03bae1
Tienanh325 <tienanh2k55@gmail.com>
2026-03-24 22:17:58 +0700

    ve tau hoa admin

--- Frontend/my-react/src/pages/VeTauHoa.tsx
+++ Frontend/my-react/src/pages/VeTauHoa.tsx
@@ -1,11 +1,11 @@
-// import TicketAdminPage from "./TicketAdminPage";
+import TicketAdminPage from "./TicketAdminPage";
 
-// export default function VeTauHoa() {
-//   return (
-//     <TicketAdminPage
-//       category="train"
-//       title="Quản lý vé tàu hoả"
-//       description="Quản lý danh sách vé tàu hoả, điểm đi - điểm đến, lịch khởi hành và số chỗ còn lại."
-//     />
-//   );
-// }
+export default function VeTauHoa() {
+  return (
+    <TicketAdminPage
+      category="train"
+      title="Quản lý vé tàu hoả"
+      description="Quản lý danh sách vé tàu hoả, điểm đi - điểm đến, lịch khởi hành và số chỗ còn lại."
+    />
+  );
+}
