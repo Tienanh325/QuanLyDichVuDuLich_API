780370d2a08ad8e20fd2176bbc720a2be59731a0
Tienanh325 <tienanh2k55@gmail.com>
2026-03-24 22:16:44 +0700

    ve khu vui choi admin

--- Frontend/my-react/src/pages/VeKhuVuiChoi.tsx
+++ Frontend/my-react/src/pages/VeKhuVuiChoi.tsx
@@ -1,11 +1,11 @@
-// import TicketAdminPage from "./TicketAdminPage";
+import TicketAdminPage from "./TicketAdminPage";
 
-// export default function VeKhuVuiChoi() {
-//   return (
-//     <TicketAdminPage
-//       category="park"
-//       title="Quản lý vé khu vui chơi"
-//       description="Quản lý vé vui chơi, cổng vào, khu trải nghiệm, giá vé và số chỗ phục vụ theo ngày."
-//     />
-//   );
-// }
+export default function VeKhuVuiChoi() {
+  return (
+    <TicketAdminPage
+      category="park"
+      title="Quản lý vé khu vui chơi"
+      description="Quản lý vé vui chơi, cổng vào, khu trải nghiệm, giá vé và số chỗ phục vụ theo ngày."
+    />
+  );
+}
