f93ef920968ccc08126409f27e4ad14a8bc50bda
Tienanh325 <tienanh2k55@gmail.com>
2026-03-24 22:17:25 +0700

    ve may bay admin

--- Frontend/my-react/src/pages/VeMayBayt.tsx
+++ Frontend/my-react/src/pages/VeMayBayt.tsx
@@ -1,11 +1,11 @@
-// import TicketAdminPage from "./TicketAdminPage";
+import TicketAdminPage from "./TicketAdminPage";
 
-// export default function VeMayBay() {
-//   return (
-//     <TicketAdminPage
-//       category="flight"
-//       title="Quản lý vé máy bay"
-//       description="Theo dõi lịch bay, giá vé, số chỗ trống và đánh giá cho nhóm vé máy bay."
-//     />
-//   );
-// }
+export default function VeMayBay() {
+  return (
+    <TicketAdminPage
+      category="flight"
+      title="Quản lý vé máy bay"
+      description="Theo dõi lịch bay, giá vé, số chỗ trống và đánh giá cho nhóm vé máy bay."
+    />
+  );
+}
